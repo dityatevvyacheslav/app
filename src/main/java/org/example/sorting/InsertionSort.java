1785360a61f463df770b0cbd42563f4c3c5ae92a
DESKTOP-OR2UVR1\Home <xia_kun.08@mail.ru>
2025-10-27 15:33:22 +0500

    Sorting: BubbleSort, InsertionSort, MergeSort, MergeSortNumericField, RadixSort and test

--- src/main/java/org/example/sorting/InsertionSort.java
+++ src/main/java/org/example/sorting/InsertionSort.java
@@ -1,6 +1,25 @@
 package org.example.sorting;
 
-public class InsertionSort {
+public class InsertionSort<T extends Comparable<? super T>> {
+    public void sort(T[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+
+        }
+    }
+
     public void sort(int[] array) {
         if (array == null || array.length <= 1) {
             return;
